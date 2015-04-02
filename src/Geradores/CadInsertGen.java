281f0628cd52facd93840b5cb0f84f0548a06784
Guto Leoni <Guto Leoni@Guto>
2015-04-02 10:23:49 -0300

    Adicionando javadoc

--- src/Geradores/CadInsertGen.java
+++ src/Geradores/CadInsertGen.java
@@ -26,7 +26,11 @@ public class CadInsertGen {
         this.caminho_dir = caminho_dir;
     }
     
-    
+    /**
+     * Gerador do código PHP que irá inserir dados no banco de dados.
+     * @param O nome do banco de dados com qual o programa irá trabalhar
+     * @throws IOException 
+     */
     
      public void gerarCadInserir(String nome_banco) throws IOException{
     
@@ -259,7 +263,12 @@ public class CadInsertGen {
     }//fim do método de gerar o código em PHP
     
 	
-	
+	/**
+         * Método que escreve no arquivo o script que conect com o banco de dados.
+         * @param construtor que recebe como argumento o objeto do tipo FileWriter
+         * @param string com o nome do banco de dados
+         * @throws IOException 
+         */
 	public void gerarConexao(BufferedWriter bw, String nome_banco) throws IOException{
     
         bw.write("<?php");
@@ -281,7 +290,11 @@ public class CadInsertGen {
     
     }
         
-        
+        /**
+         * Médoto para escrever o nome de uma coluna em um arquivo. 
+         * @param construtor que recebe como argumento o objeto do tipo FileWriter
+         * @param string que será escrita (no caso, o nome da coluna) 
+         */
         public void escreverColuna(BufferedWriter bw,String s){
             
         try {
