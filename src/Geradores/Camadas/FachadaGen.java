ddd2be3e463951af18d0da7ebaccb93875ab29d7
Renan Felix <renanfelixrodrigues@hotmail.com>
2015-05-30 09:42:42 -0300

    POKEMAUN

--- src/Geradores/Camadas/FachadaGen.java
+++ src/Geradores/Camadas/FachadaGen.java
@@ -87,10 +87,17 @@ public class FachadaGen {
     
         
         
-        //ok agora vai
-        //ok, teste de merge mano
-        
-        //AGORA É 
+        /*
+        I wanna be the very best
+        Like no one ever was
+        To catch them is my real test
+        To train them is my cause
+
+        I will travel across the land
+        Searching far and wide
+        Teach Pokémon to understand[1]
+        The power that's inside
+        */
         
         return true;
     
