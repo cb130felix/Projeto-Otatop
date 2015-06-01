a87c121269d16101639824d461ac6b6ee5e8d340
Arthur Flôr <arthurflor23@gmail.com>
2015-06-01 14:01:07 -0300

    Documentação - Arquivos auxiliares

--- src/Auxiliares/CopyFile.java
+++ src/Auxiliares/CopyFile.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Auxiliares;
 
 import java.io.File;
@@ -13,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- *
+ * Classe reponsável por copiar arquivo (biblioteca).
  * @author Renan
  */
 public class CopyFile {
--- src/Auxiliares/FixString.java
+++ src/Auxiliares/FixString.java
@@ -63,9 +63,4 @@ public class FixString {
     
     return nomeMetodo;
     }
-    
-    
-    
-    
-    
 }
